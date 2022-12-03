b1017609c25730d41ea5eff3a0a1a03d77be4e9a
aniketp02 <pokleaniket92@gmail.com>
2022-12-03 12:30:54 +0530

    Solidity Contract v2

--- contracts/Tunes.sol
+++ contracts/Tunes.sol
@@ -3,37 +3,35 @@ pragma solidity ^0.8.17;
 
 contract Tunes{
     struct addSong{
+        string ipfsHash; //unique identifier
         uint256 songId;
-        string ipfsHash;
-        string songMeta; 
+        string songName; 
+        string songArtist;
         address owner;
-        uint256 cost;
         bool isStored;
     }
 
-
-    mapping(uint256 => addSong) audioFiles;  
-    mapping(uint256 => uint256) songLikes;
-    mapping(uint256 => uint256) songDislikes;
-    // mapping(uint256 => uint256) songDownloads;
+    mapping(uint256 => string) idHashMap;
+    mapping(string => addSong) audioFiles;  
+    mapping(string => uint256) songLikes;
 
     uint songCounts;
 
     event fileStored(address indexed owner, uint256 indexed songId);
-    // event downloaded(address indexed streamer, uint256 songId);
     event success(uint256 indexed songid, string message);
     event paymentSuccess(address indexed streamer, address indexed songOwner, uint256 cost);
 
     function addFile(
+        string memory _ipfsHash,
         uint256 _id,
-        string memory _ipfsHash, 
-        string memory _songMeta, 
-        uint256 _cost) external returns(bool){
+        string memory _songName,
+        string memory _songArtist
+        ) external returns(bool){
 
-        require(audioFiles[_id].isStored == false, "This file already exists! copyright alert!!");
+        require(audioFiles[_ipfsHash].isStored == false, "This file already exists! copyright alert!!");
 
-        audioFiles[_id] = addSong(_id, _ipfsHash, _songMeta, msg.sender, _cost, true);
-
+        audioFiles[_ipfsHash] = addSong(_ipfsHash, _id, _songName, _songArtist, msg.sender, true);
+        idHashMap[_id] = _ipfsHash;
         songCounts++;
         
         emit fileStored(msg.sender, _id);
@@ -41,26 +39,22 @@ contract Tunes{
     }
 
     function donate(uint256 _id) external payable{
-        address songOwner = audioFiles[_id].owner;
+        address songOwner = audioFiles[idHashMap[_id]].owner;
         payable(songOwner).transfer(msg.value);
         emit paymentSuccess(msg.sender, songOwner, msg.value);
     }
 
-    // function getDownloads(uint256 _id) external view returns(uint256){
-    //     return songDownloads[_id];
-    // }
-
     function likeSong(uint256 _id) external{
-        songLikes[_id] += 1;
+        songLikes[idHashMap[_id]] += 1;
         emit success(_id, "Updated likes for the song!");
     }
     
     function getLikes(uint256 _id) external view returns(uint256){
-        return songLikes[_id];
+        return songLikes[idHashMap[_id]];
     }
 
-    function getHash(uint256 _id) external view returns(string memory){
-        return audioFiles[_id].ipfsHash;
+    function getHash(uint256 _id) public view returns(string memory){
+        return audioFiles[idHashMap[_id]].ipfsHash;
     }
 
     function getTotalSongs() external view returns(uint256){
@@ -69,8 +63,9 @@ contract Tunes{
 
     function getSongs() external view returns(addSong[] memory){
         addSong[] memory ret = new addSong[](songCounts);
-        for (uint i = 0; i < songCounts; i++) {
-            ret[i] = audioFiles[i];
+        for (uint i = 1; i < songCounts; i++) {
+            string memory songHash = getHash(i);
+            ret[i] = audioFiles[songHash];
         }
         return ret;
     }
